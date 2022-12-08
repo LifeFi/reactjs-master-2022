7c1cc2cdf58fabf34dde666452aaa02963852761
topcircle <topcircle@gmail.com>
2022-12-08 17:27:07 +0900

    2.3

--- src/App.js
+++ src/App.js
@@ -4,25 +4,26 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
-
-const Text = styled.span`
+const Btn = styled.button`
   color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`;
+
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
     <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+
+      <input />
     </Father>
   );
 }
