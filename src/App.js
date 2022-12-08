f0ff6807e2978107989fe4b0a11121b2b3ae794f
topcircle <topcircle@gmail.com>
2022-12-08 18:17:41 +0900

    2.7

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
 const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   width: 100vw;
   justify-content: center;
   align-items: center;
-`;
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const BigCircle = styled(Box)`
-  border-radius: 50%;
-  width: 200px;
-  height: 200px;
-`;
-
-const Input = styled.input.attrs({ required: true })`
-  color: tomato;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box bgColor="teal"></Box>
-      <Box bgColor="tomato"></Box>
-      <BigCircle bgColor="tomato"></BigCircle>
+      <Title>Hi</Title>
     </Wrapper>
   );
 }
