5c52ececbd079b59e554cca6fddb912d49e2074e
topcircle <topcircle@gmail.com>
2022-12-09 15:09:40 +0900

    5.10

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 //ts에서 오류 발생해서 ! 추가함.
@@ -10,8 +13,10 @@ const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
   //<React.StrictMode> //리스트에서 클릭시, 페이지 아동이 안되는 문제 때문에 없앰
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </QueryClientProvider>
   //</React.StrictMode>
 );
