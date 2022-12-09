e04848f3ddf1155ea89af319b384fa3ed2695b7d
topcircle <topcircle@gmail.com>
2022-12-09 16:48:36 +0900

    5.15

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,5 +1,7 @@
 // import { useState, useEffect } from "react";
 import styled from "styled-components";
+import { Helmet } from "react-helmet";
+
 import {
   Switch,
   Route,
@@ -160,7 +162,10 @@ function Coin() {
   );
   const { isLoading: tickersLoading, data: tickersData } = useQuery<PriceData>(
     ["tickers", coinId],
-    () => fetchCoinTickers(coinId)
+    () => fetchCoinTickers(coinId),
+    {
+      refetchInterval: 500,
+    }
   );
 
   // const [loading, setLoading] = useState(true);
@@ -184,6 +189,11 @@ function Coin() {
 
   return (
     <Container>
+      <Helmet>
+        <title>
+          {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
+        </title>
+      </Helmet>
       <Header>
         <Title>
           {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
@@ -203,8 +213,8 @@ function Coin() {
               <span>${infoData?.symbol}</span>
             </OverviewItem>
             <OverviewItem>
-              <span>Open Source:</span>
-              <span>{infoData?.open_source ? "Yes" : "No"}</span>
+              <span>Price:</span>
+              <span>{tickersData?.quotes.USD.price.toFixed(3)}</span>
             </OverviewItem>
           </Overview>
           <Description>{infoData?.description}</Description>
@@ -233,7 +243,7 @@ function Coin() {
               <Price />
             </Route>
             <Route path={`/:coinId/chart`}>
-              <Chart />
+              <Chart coinId={coinId} />
             </Route>
           </Switch>
         </>
